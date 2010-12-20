62ee93f276fd965f35c5664137df9486e71a7d8a
Johannes Donath <akkarin@develfusion.com>
2010-12-20 02:44:56 +0100

    Added comment at top of file

--- tar/files/lib/data/search/PackageType.class.php
+++ tar/files/lib/data/search/PackageType.class.php
@@ -2,21 +2,33 @@
 // wcf imports
 require_once(WCF_DIR.'lib/data/search/SearchType.class.php');
 
-
+/**
+ * Provides methods for searching the package database
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
 class PackageType extends SearchType {
+
+	/**
+	 * @see	SearchType::$searchableFields
+	 */
 	protected $searchableFields = array('packageName', 'description', 'plugin', 'licence', 'licenceUrl');
+
+	/**
+	 * @see	SearchType::$advancedSearchFields
+	 */
 	protected $advancedSearchFields = array('name');
-	
+
 	/**
 	 * @see SearchType::formatResult()
 	 */
 	protected function formatResult($row) {
 		$row['versions'] = unserialize($row['versions']);
-		
+
 		$result = array('ID' => $row['packageID'], 'title' => $row['packageName']." (".$row['name'].")", 'description' => $row['description'],  'additionalButtons' => '', 'suggestionField' => $row['packageName'], 'result' => $row);
-		
+
 		$result['additionalButtons'] = 'packageSearchTypeButtons';
-		
+
 		return $result;
 	}
 }
