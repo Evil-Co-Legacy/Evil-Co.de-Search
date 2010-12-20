96b7379b8e697c317db7275ccc14bf2e9a6ab45c
Johannes Donath <akkarin@develfusion.com>
2010-12-20 03:19:57 +0100

    Implemented SearchResult (This class will represent a search result)

---
+++ requirements/de.evil-co.search.core/tar/files/lib/data/search/SearchResult.class.php
@@ -0,0 +1,49 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/data/DatabaseObject.class.php');
+
+/**
+ * Provides default methods for search results
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
+class SearchResult extends DatabaseObject {
+
+	/**
+	 * Creates a new search result instance
+	 * @param	array	$row
+	 */
+	public function __construct($row) {
+		// handle result set
+		parent::__construct($row);
+	}
+
+	/**
+	 * Returnes the ID of the current result
+	 */
+	abstract public function getResultID();
+
+	/**
+	 * Returnes the title of the current result
+	 */
+	abstract public function getTitle();
+
+	/**
+	 * Returnes the description of the current result
+	 */
+	abstract public function getDescription();
+
+	/**
+	 * Returnes additional buttons for the current result
+	 */
+	abstract public function getAdditionalButtons();
+
+	/**
+	 * Returnes the template name of detail template for current result
+	 * Note: If this returnes false the link to detail page will disappear
+	 */
+	public function getDetailTemplate() {
+		return false;
+	}
+}
+?>
