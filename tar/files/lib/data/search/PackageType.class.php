0dac5bd99a85b54b5867aaa5dda8598cc1a6ae5f
Johannes Donath <akkarin@develfusion.com>
2010-12-20 04:16:09 +0100

    Updated PackageType for new table layout

--- tar/files/lib/data/search/PackageType.class.php
+++ tar/files/lib/data/search/PackageType.class.php
@@ -1,6 +1,7 @@
 <?php
 // wcf imports
 require_once(WCF_DIR.'lib/data/search/SearchType.class.php');
+require_once(WCF_DIR.'lib/data/search/SearchResult.class.php');
 
 /**
  * Provides methods for searching the package database
@@ -12,7 +13,7 @@ class PackageType extends SearchType {
 	/**
 	 * @see	SearchType::$searchableFields
 	 */
-	protected $searchableFields = array('packageName', 'description', 'plugin', 'licence', 'licenceUrl');
+	protected $searchableFields = array('language.packageName', 'language.description', 'version.plugin', 'version.licence', 'version.licenceUrl');
 
 	/**
 	 * @see	SearchType::$advancedSearchFields
@@ -20,16 +21,75 @@ class PackageType extends SearchType {
 	protected $advancedSearchFields = array('name');
 
 	/**
-	 * @see SearchType::formatResult()
+	 * @see	SearchType::$searchResultClass
 	 */
-	protected function formatResult($row) {
-		$row['versions'] = unserialize($row['versions']);
+	protected $searchResultClass = 'PackageResult';
 
-		$result = array('ID' => $row['packageID'], 'title' => $row['packageName']." (".$row['name'].")", 'description' => $row['description'],  'additionalButtons' => '', 'suggestionField' => $row['packageName'], 'result' => $row);
+	/**
+	 * @see SearchType::executeSearchQuery()
+	 */
+	protected function executeSearchQuery($sqlConditions) {
+		$sql = "SELECT
+					package.packageID AS packageID,
+					package.packageName AS packageName,
+					language.name AS name,
+					language.description AS description
+				FROM
+					www".WWW_N."_package package
+				LEFT JOIN
+					www".WWW_N."_package_version version
+				ON
+					package.lastVersionID = version.versionID
+				LEFT JOIN
+					".WWW_N."_package_version_to_language language
+				ON
+					version.versionID = language.versionID
+				WHERE
+					(
+							language.languageID = 1
+						OR
+							language.isFallback = 1
+					)
+				AND
+					".$sqlConditions;
+		$result = WCF::getDB()->sendQuery($sql, $itemsPerPage, (($page - 1) * $itemsPerPage));
 
-		$result['additionalButtons'] = 'packageSearchTypeButtons';
+		// create needed array
+		$resultList = array();
 
-		return $result;
+		// loop while fetching rows
+		while ($row = WCF::getDB()->fetchArray($result)) {
+			$resultList[] = new $this->searchResultClass($row);
+		}
+
+		// get count of all matching results
+		$sql = "SELECT
+					COUNT(*) AS count
+				FROM
+					www".WWW_N."_package package
+				LEFT JOIN
+					www".WWW_N."_package_version version
+				ON
+					package.lastVersionID = version.versionID
+				LEFT JOIN
+					".WWW_N."_package_version_to_language language
+				ON
+					version.versionID = language.versionID
+				WHERE
+					(
+							language.languageID = 1
+						OR
+							language.isFallback = 1
+					)
+				AND
+					".$sqlConditions;
+		$count = WCF::getDB()->getFirstRow($sql);
+
+		// write count to class property
+		$this->lastSearchCount = intval($count['count']);
+
+		// return list of results
+		return $resultList;
 	}
 }
 ?>
