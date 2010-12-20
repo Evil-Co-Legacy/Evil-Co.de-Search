fdd7754a7dacaec8138495ca91b6580c55db62f8
Johannes Donath <akkarin@develfusion.com>
2010-12-20 07:00:27 +0100

    Added the new table for cron update types

--- requirements/de.evil-co.search.core/install/install.sql
+++ requirements/de.evil-co.search.core/install/install.sql
@@ -8,3 +8,10 @@ CREATE TABLE `wcf1_search_type` (
 	`isInDevelelopment` TINYINT (1) NOT NULL DEFAULT '0',
 	`isDisabled` TINYINT (1) NOT NULL DEFAULT '0'
 );
+
+DROP TABLE IF EXISTS `wcf1_cron_update_type`;
+CREATE TABLE `wcf1_cron_update_type` (
+	`typeID` INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`packageID` INT NOT NULL DEFAULT '0',
+	`file` TEXT NOT NULL
+);
