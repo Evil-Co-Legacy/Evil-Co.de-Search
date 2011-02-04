62c28c70df93f5c1b7447e2c55e263386490725a
Johannes Donath <akkarin@develfusion.com>
2011-02-04 18:30:16 +0100

    Corrected tabs

--- tar/files/lib/data/search/PackageType.class.php
+++ tar/files/lib/data/search/PackageType.class.php
@@ -31,46 +31,46 @@ class PackageType extends SearchType {
 	protected function executeSearchQuery($sqlConditions, $additionalSelects) {
 		// wuhahahaha ... monster query from hell :-D
 		$sql = "SELECT
-					package.packageID AS packageID,
-					package.packageName AS packageName,
-					language.name AS name,
-					language.description AS description,
-					server.serverID AS serverID,
-					server.serverAlias AS serverAlias,
-					server.serverUrl AS serverUrl
-					version.version AS version,
-					version.isUnique AS isUnique,
-					version.standalone AS standalone,
-					version.plugin AS plugin,
-					version.packageUrl AS packageUrl,
-					version.author AS author,
-					version.authorUrl AS authorUrl
-					".(!empty($additionalSelects) ? ','.$additionalSelects : "")."
-				FROM
-					www".WWW_N."_package package
-				LEFT JOIN
-					www".WWW_N."_package_version version
-				ON
-					package.lastVersionID = version.versionID
-				LEFT JOIN
-					www".WWW_N."_package_version_to_language language
-				ON
-					version.versionID = language.versionID
-				LEFT JOIN
-					www".WWW_N."_package_server server
-				ON
-					version.serverID = server.serverID
-				WHERE
-					(
-							language.languageID = 1
-						OR
-							language.isFallback = 1
-					)
-				AND
-					".$sqlConditions."
-				ORDER BY
-					INET_ATON(SUBSTRING_INDEX(CONCAT(version.version,'.0.0.0'),'.',4)) DESC,
-					searchScore ASC";
+				package.packageID AS packageID,
+				package.packageName AS packageName,
+				language.name AS name,
+				language.description AS description,
+				server.serverID AS serverID,
+				server.serverAlias AS serverAlias,
+				server.serverUrl AS serverUrl
+				version.version AS version,
+				version.isUnique AS isUnique,
+				version.standalone AS standalone,
+				version.plugin AS plugin,
+				version.packageUrl AS packageUrl,
+				version.author AS author,
+				version.authorUrl AS authorUrl
+				".(!empty($additionalSelects) ? ','.$additionalSelects : "")."
+			FROM
+				www".WWW_N."_package package
+			LEFT JOIN
+				www".WWW_N."_package_version version
+			ON
+				package.lastVersionID = version.versionID
+			LEFT JOIN
+				www".WWW_N."_package_version_to_language language
+			ON
+				version.versionID = language.versionID
+			LEFT JOIN
+				www".WWW_N."_package_server server
+			ON
+				version.serverID = server.serverID
+			WHERE
+				(
+						language.languageID = 1
+					OR
+						language.isFallback = 1
+				)
+			AND
+				".$sqlConditions."
+			ORDER BY
+				INET_ATON(SUBSTRING_INDEX(CONCAT(version.version,'.0.0.0'),'.',4)) DESC,
+			searchScore ASC";
 		$result = WCF::getDB()->sendQuery($sql, $itemsPerPage, (($page - 1) * $itemsPerPage));
 
 		// create needed array
@@ -83,25 +83,25 @@ class PackageType extends SearchType {
 
 		// get count of all matching results
 		$sql = "SELECT
-					COUNT(*) AS count
-				FROM
-					www".WWW_N."_package package
-				LEFT JOIN
-					www".WWW_N."_package_version version
-				ON
-					package.lastVersionID = version.versionID
-				LEFT JOIN
-					".WWW_N."_package_version_to_language language
-				ON
-					version.versionID = language.versionID
-				WHERE
-					(
-							language.languageID = 1
-						OR
-							language.isFallback = 1
-					)
-				AND
-					".$sqlConditions;
+				COUNT(*) AS count
+			FROM
+				www".WWW_N."_package package
+			LEFT JOIN
+				www".WWW_N."_package_version version
+			ON
+				package.lastVersionID = version.versionID
+			LEFT JOIN
+				".WWW_N."_package_version_to_language language
+			ON
+				version.versionID = language.versionID
+			WHERE
+				(
+						language.languageID = 1
+					OR
+						language.isFallback = 1
+				)
+			AND
+				".$sqlConditions;
 		$count = WCF::getDB()->getFirstRow($sql);
 
 		// write count to class property
