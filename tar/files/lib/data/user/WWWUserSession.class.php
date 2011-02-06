c06b778cc4037184a147e0792553c379a9b503b7
Johannes Donath <akkarin@develfusion.com>
2011-02-06 02:09:42 +0100

    Added getAvatar method

--- tar/files/lib/data/user/WWWUserSession.class.php
+++ tar/files/lib/data/user/WWWUserSession.class.php
@@ -3,5 +3,12 @@ require_once(WWW_DIR.'lib/data/user/AbstractWWWUserSession.class.php');
 
 class WWWUserSession extends AbstractWWWUserSession {
 	
+	/**
+	 * Returns the avatar of this user.
+	 * @return	DisplayableAvatar
+	 */
+	public function getAvatar() {
+		return $this->avatar;
+	}
 }
 ?>
