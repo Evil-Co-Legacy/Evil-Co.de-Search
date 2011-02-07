e9b1704c6a3e07a80211bb19e494cf9ad47e9c9f
Johannes Donath <akkarin@develfusion.com>
2011-02-08 00:54:35 +0100

    Created ModerationOverviewPage

---
+++ tar/files/lib/page/ModerationOverviewPage.class.php
@@ -0,0 +1,53 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/page/AbstractPage.class.php');
+
+/**
+ * Displays a moderation overview
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class ModerationOverview extends AbstractPage {
+	
+	/**
+	 * @see Page::$templateName
+	 * @var string
+	 */
+	public $templateName = 'moderationOverview';
+	
+	/**
+	 * Contains a count of outstanding server requests
+	 * @var integer
+	 */
+	public $outstandingServerRequests = 0;
+	
+	/**
+	 * @see Page::readData()
+	 */
+	public function readData() {
+		parent::readData();
+		
+		$sql = "SELECT
+				COUNT(*) AS count
+			FROM
+				www".WWW_N."_package_server_request
+			WHERE
+				state = 'waiting'";
+		$row = WCF::getDB()->getFirstRow($sql);
+			
+		$this->outstandingServerRequests = $row['count'];
+	}
+	
+	/**
+	 * @see Page::assignVariables()
+	 */
+	public function assignVariables() {
+		parent::assignVariables();
+		
+		WCF::getTPL()->assign(array(
+			'outstandingServerRequests'		=>	$this->outstandingServerRequests
+		));
+	}
+}
+?>
