dafa2f5e3dcc37eb36ba8f708d887c4b52125899
Johannes Donath <akkarin@develfusion.com>
2011-02-08 01:52:47 +0100

    Created ModerationPckageServerRequestPage
    Change-Id: I33f2f240a54af11a832e0197df2b72d5344de6f8

---
+++ tar/files/lib/page/ModerationPackageServerRequestPage.class.php
@@ -0,0 +1,64 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/page/AbstractPage.class.php');
+
+/**
+ * Displays a list of server requests
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class ModerationPackageServerRequestPage extends AbstractPage {
+	
+	/**
+	 * @see AbstractPage::$templateName
+	 */
+	public $templateName = 'moderationPackageServerRequest';
+	
+	/**
+	 * Contains waiting requests
+	 * @var array
+	 */
+	public $requests = array();
+	
+	/**
+	 * @see Page::readData()
+	 */
+	public function readData() {
+		parent::readData();
+		
+		$sql = "SELECT
+				*
+			FROM
+				www".WWW_N."_package_server_request
+			".(!isset($_REQUEST['showOthers']) ? "WHERE state = 'waiting'" : "");
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		while($row = WCF::getDB()->sendQuery($sql)) {
+			$this->requests[] = $row;
+		}
+	}
+	
+	/**
+	 * @see Page::show()
+	 */
+	public function show() {
+		// activate usercpmenu
+		require_once(WCF_DIR.'lib/page/util/menu/UserCPMenu.class.php');
+		UserCPMenu::getInstance()->setActiveMenuItem('www.user.usercp.menu.link.modcp.packageServerRequests');
+		
+		parent::show();
+	}
+	
+	/**
+	 * @see Page::assignVariables()
+	 */
+	public function assignVariables() {
+		parent::assignVariables();
+		
+		WCF::getTPL()->assign(array(
+			'requests'		=>	$this->requests
+		));
+	}
+}
+?>
