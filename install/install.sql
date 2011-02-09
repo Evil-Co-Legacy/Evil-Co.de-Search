15196312d20850b5f8a674eb0f8fd5167aa3d82f
Johannes Donath <akkarin@develfusion.com>
2011-02-09 01:58:36 +0100

    Added report table

--- install/install.sql
+++ install/install.sql
@@ -6,6 +6,15 @@ CREATE TABLE `www1_1_package` (
 	`lastVersionID` INT NULL
 );
 
+DROP TABLE IF EXISTS `www1_1_package_report`;
+CREATE TABLE `www1_1_package_report` (
+	`reportID` INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`packageID` INT NOT NULL,
+	`authorID` INT NOT NULL,
+	`authorName` VARCHAR (255) NOT NULL,
+	`reason` TEXT NOT NULL
+);
+
 DROP TABLE IF EXISTS `www1_1_package_mirror`;
 CREATE TABLE `www1_1_package_mirror` (
 	`packageID` INT NOT NULL,
