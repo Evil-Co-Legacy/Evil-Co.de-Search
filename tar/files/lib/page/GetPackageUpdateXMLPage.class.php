9cb4914e2502e32fb43e264cf674d0166373c6d9
Johannes Donath <akkarin@develfusion.com>
2011-02-10 05:36:54 +0100

    Created GetPackageUpdateXMLPage.class.php Created template for package update xml page

---
+++ tar/files/lib/page/GetPackageUpdateXMLPage.class.php
@@ -0,0 +1,270 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/page/AbstractPage.class.php');
+
+/**
+ * Displays a meta package server
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class GetPackageUpdateXMLPage extends AbstractPage {
+	
+	/**
+	 * @see AbstractPage::$templateName
+	 */
+	public $templateName = 'getPackageUpdateXML';
+	
+	/**
+	 * Contains all packages
+	 * @var array
+	 */
+	public $resultList = array();
+	
+	/**
+	 * @see Page::readData()
+	 */
+	public function readData() {
+		parent::readData();
+		
+		$sql = "SELECT
+				package.packageID,
+				package.packageName,
+				packageLanguage.name AS name,
+				packageLanguage.description,
+				server.serverID,
+				server.serverAlias,
+				server.serverUrl,
+				version.versionID,
+				version.version,
+				version.isUnique,
+				version.standalone,
+				version.plugin,
+				version.packageUrl,
+				version.author,
+				version.authorUrl,
+				version.licenseName,
+				version.licenseUrl,
+				version.downloadUrl,
+				mirror.isEnabled AS mirrorEnabled,
+				packageLanguage.isFallback,
+				packageLanguage.languageID
+			FROM
+				www".WWW_N."_package package
+			LEFT JOIN
+				www".WWW_N."_package_version version
+			ON
+				package.lastVersionID = version.versionID
+			LEFT JOIN
+				www".WWW_N."_package_version_to_language packageLanguage
+			ON
+				version.versionID = packageLanguage.versionID
+			LEFT JOIN
+				www".WWW_N."_package_server server
+			ON
+				version.serverID = server.serverID
+			LEFT JOIN
+				www".WWW_N."_package_mirror AS mirror
+			ON
+				(package.packageID = package.packageID AND version.versionID = mirror.versionID)
+			WHERE
+				(
+						packageLanguage.languageID = ".WCF::getLanguage()->getLanguageID()."
+					OR
+						packageLanguage.isFallback = 1
+				)
+			AND
+				".$sqlConditions."
+			ORDER BY
+				package.packageName ASC";
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		$resultList = array();
+		
+		while($row = WCF::getDB()->fetchArray($result)) {
+			$resultList[] = new PackageResult($row, true);
+		}
+		
+		$resultIDs = array();
+		$versionIDs = array();
+		
+		foreach($resultList as $key => $result) {
+			$resultIDs[$result->getResultID()] = $key;
+			$versionIDs[$result->versionID] = $key;
+		}
+		
+		// get requirements
+		$sql = "SELECT
+				targetPackageID AS packageID,
+				targetVersionID AS versionID,
+				packageLanguage.name AS name,
+				packageLanguage.description,
+				requirement.version,
+				requirement.packageName AS packageName,
+				requirement.packageID AS parentPackageID
+			FROM
+				www".WWW_N."_package_version_requirement requirement
+			LEFT JOIN
+				www".WWW_N."_package_version version
+			ON
+				requirement.targetVersionID = version.versionID
+			LEFT JOIN
+				www".WWW_N."_package_version_to_language packageLanguage
+			ON
+				version.versionID = packageLanguage.versionID
+			WHERE
+				(
+						packageLanguage.languageID = ".WCF::getLanguage()->getLanguageID()."
+					OR
+						packageLanguage.isFallback = 1
+				)
+			AND
+				requirement.versionID IN (".implode(',', array_keys($versionIDs)).")";
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		$requirements = array();
+		while($row = WCF::getDB()->fetchArray($result)) {
+			if (!isset($requirements[$row['parentPackageID']])) $requirements[$row['parentPackageID']] = array();
+			$requirements[$row['parentPackageID']][] = new PackageResult($row, true);
+		}
+		
+		// write arrays
+		foreach($requirements as $packageID => $requirementList) {
+			$resultList[$resultIDs[$packageID]]->requirements = $requirementList;
+		}
+		
+		// get optionals
+		$sql = "SELECT
+				targetPackageID AS packageID,
+				targetVersionID AS versionID,
+				packageLanguage.name AS name,
+				packageLanguage.description,
+				optional.version,
+				optional.packageID AS parentPackageID,
+				optional.packageName AS packageName
+			FROM
+				www".WWW_N."_package_version_optional optional
+			LEFT JOIN
+				www".WWW_N."_package_version version
+			ON
+				optional.targetVersionID = version.versionID
+			LEFT JOIN
+				www".WWW_N."_package_version_to_language packageLanguage
+			ON
+				version.versionID = packageLanguage.versionID
+			WHERE
+				(
+						packageLanguage.languageID = ".WCF::getLanguage()->getLanguageID()."
+					OR
+						packageLanguage.isFallback = 1
+				)
+			AND
+				optional.versionID IN (".implode(',', array_keys($versionIDs)).")";
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		$optionals = array();
+		while($row = WCF::getDB()->fetchArray($result)) {
+			if (!isset($optionals[$row['parentPackageID']])) $optionals[$row['parentPackageID']] = array();
+			$optionals[$row['parentPackageID']][] = new PackageResult($row, true);
+		}
+		
+		// write arrays
+		foreach($optionals as $packageID => $optionalsList) {
+			$resultList[$resultIDs[$packageID]]->optionals = $optionalsList;
+		}
+		
+		// get instructions
+		$instructions = array();
+		$sql = "SELECT
+				pipList,
+				packageID AS parentPackageID
+			FROM
+				www".WWW_N."_package_version_instruction
+			WHERE
+				versionID IN (".implode(',', array_keys($versionIDs)).")
+			AND
+				instructionType = 'install'"; // TODO: we should add support for all types
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		while($row = WCF::getDB()->fetchArray($result)) {
+			$instructions[$row['parentPackageID']] = explode(',', $row['pipList']);
+		}
+		
+		foreach($instructions as $packageID => $pipList) {
+			$resultList[$resultIDs[$packageID]]->instructions = $pipList;
+		}
+		
+		// get update instructions
+		$updateInstructions = array();
+		$sql = "SELECT
+				pipList,
+				packageID AS parentPackageID,
+				fromVersion
+			FROM
+				www".WWW_N."_package_version_instruction
+			WHERE
+				versionID IN (".implode(',', array_keys($versionIDs)).")
+			AND
+				instructionType = 'update'";
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		while($row = WCF::getDB()->fetchArray($result)) {
+			$updateInstructions[$row['parentPackageID']][$row['fromVersion']] = explode(',', $row['pipList']);
+		}
+		
+		foreach($instructions as $packageID => $pipList) {
+			$resultList[$resultIDs[$packageID]]->updateInstructions = $pipList;
+		}
+		
+		// get versions
+		$versions = array();
+		$sql = "SELECT
+				version.*,
+				mirror.isEnabled AS mirrorEnabled
+			FROM
+				www".WWW_N."_package_version version
+			LEFT JOIN
+				www".WWW_N."_package_mirror mirror
+			ON
+				version.versionID = mirror.versionID
+			WHERE
+				version.packageID IN (".implode(',', array_keys($resultIDs)).")
+			ORDER BY
+				version.version DESC";
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		while($row = WCF::getDB()->fetchArray($result)) {
+			if (!isset($versions[$row['packageID']])) $versions[$row['packageID']] = array();
+			$versions[$row['packageID']][] = $row;
+		}
+		
+		foreach($instructions as $packageID => $versionList) {
+			$resultList[$resultIDs[$packageID]]->versions = $versionList;
+		}
+		
+		$this->resultList = $resultList;
+	}
+	
+	/**
+	 * @see Page::show()
+	 */
+	public function show() {
+		// send header
+		header("Content-type: text/xml");
+		
+		// display template
+		WCF::getTPL()->display($this->templateName);
+	}
+	
+	/**
+	 * @see Page::assignVariables()
+	 */
+	public function assignVariables() {
+		parent::assignVariables();
+		
+		WCF::getTPL()->assign(array(
+			'resultList'	=>	$this->resultList
+		));
+	}
+}
+?>
---
+++ tar/templates/getPackageUpdateXML.tpl
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<section name="packages">
+	{foreach from=$resultList item='result'}
+		<package name="{$result->packageName}">
+			<packageInformation>
+				<packageName><![CDATA[{$result->getTitle()}]]></packageName>
+				<packageDescription><![CDATA[{$result->getDescription()}]]></packageDescription>
+				{if $result->plugin != ''}<plugin><![CDATA[{$result->plugin}]]></plugin>{/if}
+				{if $result->standalone}<standalone>1</standalone>{/if}
+				{if $result->isUnique}<isUnique>1</isUnique>{/if}
+			</packageInformation>
+			
+			{if $result->licenseName != '' || $result->licenseUrl != ''}
+				<licenseInformation>
+					{if $result->licenseName != ''}<license><![CDATA[{$result->licenseName}]]></license>{/if}
+					{if $result->licenseUrl != ''}<licenseUrl><![CDATA[{$result->licenseUrl}]]></license>{/if}
+				</licenseInformation>
+			{/if}
+			
+			{if $result->author != '' || $result->authorUrl}
+				<authorInformation>
+					{if $result->author != ''}<author><![CDATA[{$result->author}]]></author>{/if}
+					{if $result->authorUrl != ''}<authorUrl><![CDATA[{$result->authorUrl}]]></authorUrl>{/if}
+				</authorInformation>
+			{/if}
+			
+			<versions>
+				{foreach from=$result->getVersions() item='version'}
+					<version name="{$version.version}">
+						{if $result->updateInstructions|count}
+							<fromVersions>
+								{foreach from=$result->updateInstructions item='pipList' key='fromVersion'}
+									<fromVersion><![CDATA[{$fromVersion}]]></fromVersion>
+								{/foreach}
+							</fromVersions>
+						{/if}
+						
+						{if $result->getRequirements()|count}
+							<requiredPackages>
+								{foreach from=$result->getRequirements() item='requirement'}
+									<requiredPackage{if $requirement->version != ''} minversion="{$requirement->version}"{/if}>{$requirement->packageName}</requiredPackage>
+								{/foreach}
+							</requiredPackages>
+						{/if}
+						
+						{* TODO: Add excluded packages here *}
+						
+						{* I think this should not be hardcoded ... *}
+						<updateType><![CDATA[update]]></updateType>
+						
+						{* TODO: Add this
+						<timestamp><![CDATA[{$version->timestamp}]]></timestamp>
+						*}
+						
+						{* TODO: Add this
+						<versionType><![CDATA[{$version->type}]]></versionType>
+						*}
+						
+						<file><![CDATA[{$version.downloadUrl}]]></file>
+					</version>
+				{/foreach}
+			</versions>
+		</package>
+	{/foreach}
+</section>
