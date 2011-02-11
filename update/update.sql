2bec0d96ff6eee347ee9d65d981d0ff012bec21f
Johannes Donath <akkarin@develfusion.com>
2011-02-11 06:23:26 +0100

    Added API-Tables to update.sql

--- update/update.sql
+++ update/update.sql
@@ -10,3 +10,27 @@ CREATE TABLE `www1_1_package_report` (
 ALTER TABLE `www1_1_package_version` ADD FULLTEXT (author);
 ALTER TABLE `www1_1_package_version` ADD FULLTEXT (authorUrl);
 ALTER TABLE `www1_1_package_version` ADD `timestamp` INT NOT NULL;
+
+DROP TABLE IF EXISTS `www1_1_api_key`;
+CREATE TABLE `www1_1_api_key` (
+	`keyID` INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`publicKey` VARCHAR (255) NOT NULL,
+	`secretKey` VARCHAR (255) NOT NULL,
+	`ownerID` INT NOT NULL,
+	`ownerName` VARCHAR (255) NOT NULL
+);
+
+DROP TABLE IF EXISTS `www1_1_api_key_whitelist`;
+CREATE TABLE `www1_1_api_key_whitelist` (
+	`keyID` INT NOT NULL,
+	`ipAddress` TEXT NULL,
+	`hostname` TEXT NULL,
+	`isEnabled` TINYINT (1) NOT NULL DEFAULT '1'
+);
+
+DROP TABLE IF EXISTS `www1_1_api_key_blacklist`;
+CREATE TABLE `www1_1_api_key_blacklist` (
+	`banID` INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`ipAddress` TEXT NULL,
+	`hostname` TEXT NULL
+);
