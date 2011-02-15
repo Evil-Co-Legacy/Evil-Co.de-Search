ff371b3efc512c5725518817ab1a37601d4a81c3
Johannes Donath <akkarin@develfusion.com>
2011-02-15 14:29:17 +0100

    Added update query to package_report table

--- tar/files/lib/action/TogglePackageAction.class.php
+++ tar/files/lib/action/TogglePackageAction.class.php
@@ -44,6 +44,14 @@ class TogglePackageAction extends AbstractAction {
 				packageID = ".$this->packageID;
 		WCF::getDB()->sendQuery($sql);
 		
+		$sql = "UPDATE
+				".WWW_N."_package_report
+			SET
+				state = 'read'
+			WHERE
+				packageID = ".$this->packageID;
+		WCF::getDB()->sendQuery($sql);
+		
 		// send redirect header
 		HeaderUtil::redirect('index.php?page=ResultDetail&resultID='.$this->packageID.'&searchTypeName=PackageType'.SID_ARG_2ND_NOT_ENCODED);
 		
