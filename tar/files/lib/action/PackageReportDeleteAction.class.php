5814b65f1a35b78069345ed70c6f5cc53c2e33f4
Johannes Donath <akkarin@develfusion.com>
2011-02-16 01:08:35 +0100

    Created PackageReportDeleteAction

---
+++ tar/files/lib/action/PackageReportDeleteAction.class.php
@@ -0,0 +1,44 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/action/AbstractAction.class.php');
+
+class PackageReportDeleteAction extends AbstractAction {
+	
+	/**
+	 * Contains the ID of the report that should deleted
+	 */
+	public $reportID = 0;
+	
+	/**
+	 * @see Action::readParameters()
+	 */
+	public function readParameters() {
+		parent::readParameters();
+		
+		// permission check
+		WCF::getUser()->checkPermission('mod.search.canModerate');
+		
+		// get value of packageID query argument
+		if (isset($_REQUEST['reportID'])) $this->reportID = intval($_REQUEST['reportID']);
+	}
+	
+	/**
+	 * @see Action::execute()
+	 */
+	public function execute() {
+		parent::execute();
+		
+		$sql = "DELETE FROM
+				www".WWW_N."_package_report
+			WHERE
+				reportID = ".$this->reportID;
+		WCF::getDB()->sendQuery($sql);
+		
+		// send redirect header
+		HeaderUtil::redirect('index.php?page=PackageReports'.SID_ARG_2ND_NOT_ENCODED);
+		
+		// fire event
+		$this->executed();
+	}
+}
+?>
