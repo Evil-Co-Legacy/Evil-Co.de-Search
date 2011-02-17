7031c0a15db41f104c253d514448ac26e8eeb38e
Johannes Donath <akkarin@develfusion.com>
2011-02-17 01:48:32 +0100

    Created PackageMirrorUtil

---
+++ tar/files/lib/util/PackageMirrorUtil.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Contains util methods for mirrored packages
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class PackageMirrorUtil {
+	
+	/**
+	 * Returnes true if the given license is blacklisted
+	 * @param	string	$licenseName
+	 * @param	string	$licenseUrl
+	 */
+	public static function isBlacklistedLicense($licenseName, $licenseUrl) {
+		$sql = "SELECT
+				*
+			FROM
+				www".WWW_N."_license_blacklist
+			WHERE
+				isEnabled = 1";
+		$result = WCF::getDB()->sendQuery($sql);
+		
+		while($row = WCF::getDB()->fetchArray($result)) {
+			if (preg_match($row['licenseRegex'], $licenseName) or preg_match($row['licenseRegex'], $licenseUrl)) return true;
+		}
+		
+		return false;
+	}
+}
+?>
